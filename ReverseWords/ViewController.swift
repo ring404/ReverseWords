af35072ec9d3c4cdc21a06b47d7883b8b2f2af61
Dimitreee <Dmitrii.priimuk@gmail.com>
2021-11-10 22:16:24 +0200

    Initial commit

--- ReverseWords/ViewController.swift
+++ ReverseWords/ViewController.swift
@@ -9,6 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textInput: UITextField!
+    @IBOutlet weak var textOutput: UITextField!
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if textInput.text != "" {
+            textOutput.text =   textInput.text?.split(separator: " ").map{String($0.reversed())}.joined(separator: " ")
+            sender.setTitle("Clear", for: .normal)
+        }
+        if sender.titleLabel?.text  == "Clear" {
+            textInput.text = ""
+            textOutput.text = ""
+            sender.setTitle("Reverse", for: .normal)
+        }
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
