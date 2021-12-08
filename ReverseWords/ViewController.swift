0cf14c13619a61883ee4d28df5c16965e3bbd233
Dimitreee <Dmitrii.priimuk@gmail.com>
2021-12-08 20:47:19 +0300

    поправлено: +Форс анврап +Расстояние до дивайдера +Сделать сабкласс вместо экстеншна +Решить сториборды или кодом верстать +Разбить сетап констрейнс на блоки +Разделить акшны и аутлеты на блоки +Использовать марки +Сделать прайватом всё что может быть прайват +Вынести константы в экстеншн и енам +Вынести цифру 16 тоже в константу +дарк грей не тот цвет +Во вью дид лоад вызывать только функции +Использовать didset для атлетов, вынести из вью дид лоад +навбар не в дидсет, а в отдельную функцию +Регуларфонт переименовать +Все стринги, цифры и уиколор вынести в константы +Алгоримт который обращает слова вынести его в отдельный класс +Создать отдельную папку для экстеншнов +На сториборде добавить дивайдер вместо экстеншна

--- ReverseWords/ViewController.swift
+++ ReverseWords/ViewController.swift
@@ -8,109 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController, UINavigationBarDelegate {
-
-    let headerFont = UIFont(name: "SFProDisplay-Bold", size: 34)
-    let regularFont = UIFont(name: "SFProDisplay-Regular", size: 17)
-    
-    @IBOutlet weak var descriptionLabel: UILabel!
-    @IBOutlet weak var reverseWordsLabel: UILabel!
-    @IBOutlet weak var navigationBar: UINavigationBar!
-    @IBOutlet weak var textInput: UITextField!
-    @IBOutlet weak var textOutput: UITextField!
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        if textInput.text != "" {
-            textOutput.text =   textInput.text?.split(separator: " ").map{String($0.reversed())}.joined(separator: " ")
-            sender.setTitle("Clear", for: .normal)
+    // MARK: - Outlets
+    @IBOutlet private weak var dividerView: UIView! { didSet { dividerView.backgroundColor = UsedColors.black }}
+    @IBOutlet private weak var descriptionLabel: UILabel! {
+        didSet {
+            descriptionLabel.textColor = UsedColors.gray
+            descriptionLabel.font = UsedFonts.labelFont
         }
-        if sender.titleLabel?.text  == "Clear" {
+    }
+    @IBOutlet private weak var reverseWordsLabel: UILabel! { didSet { reverseWordsLabel.font = UsedFonts.headerFont }}
+    @IBOutlet private weak var navigationBar: UINavigationBar!
+    @IBOutlet private weak var textInput: UITextField! { didSet { textInput.font = UsedFonts.inputFont }}
+    @IBOutlet private weak var textOutput: UITextField! { didSet { textOutput.font = UsedFonts.outputFont }}
+    @IBOutlet private weak var reverseButton: UIButton! {
+        didSet {
+            reverseButton.setTitleColor(.white, for: .highlighted)
+            reverseButton.isHighlighted = true
+        }
+    }
+    // MARK: - Actions
+    @IBAction private func buttonPressed(_ sender: UIButton) {
+        if textInput.text != "" {
+            textOutput.text =  ReverseClass.reverseString(textInput: textInput.text ?? "")
+            sender.setTitle(ButtonConditions.clear, for: .normal)
+        }
+        if sender.titleLabel?.text  == ButtonConditions.clear {
             textInput.text = ""
             textOutput.text = ""
-            sender.setTitle("Reverse", for: .normal)
+            sender.setTitle(ButtonConditions.reverse, for: .normal)
         }
-        
     }
-    
-    @IBOutlet weak var reverseButton: UIButton!
-    
-    @IBAction func editingDidBegin(_ sender: UITextField) {
-        sender.setUnderLine(color: UIColor(red: 0, green: 0.478, blue: 1, alpha: 1).cgColor)
-        reverseButton.isHighlighted = false
+    @IBAction private func editingDidBegin(_ sender: UITextField) {
+        dividerView.backgroundColor = UsedColors.blue
     }
-    
-    @IBAction func editingDidEnd(_ sender: UITextField, forEvent event: UIEvent) {
-        sender.setUnderLine(color: UIColor.darkGray.cgColor)
+    @IBAction private func editingDidEnd(_ sender: UITextField, forEvent event: UIEvent) {
+        dividerView.backgroundColor = UsedColors.black
     }
-    
-    
+    // MARK: - Functions
     override func viewDidLoad() {
         super.viewDidLoad()
+        adjustNavBar()
+    }
+    private func adjustNavBar () {
         navigationBar.delegate = self
-        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.font : regularFont!]
-        
-        descriptionLabel.textColor = UIColor(red: 0.235, green: 0.235, blue: 0.263, alpha: 0.6)
-        textInput.setUnderLine(color: UIColor.darkGray.cgColor)
-        reverseButton.isHighlighted = true
-        reverseWordsLabel.font = headerFont
-        descriptionLabel.font = regularFont
-        textInput.font = regularFont
-        textOutput.font = regularFont?.withSize(24)
-        
-      setupConstraints()
-        
-        
-        
+        UINavigationBar.appearance().titleTextAttributes = [NSAttributedString.Key.font : UsedFonts.labelFont!]
     }
-    func position(for bar: UIBarPositioning) -> UIBarPosition { return .topAttached }
-    
-    func setupConstraints () {
-        let parent = self.view!
-        reverseButton.translatesAutoresizingMaskIntoConstraints = false
-        reverseButton.heightAnchor.constraint(equalToConstant: 56).isActive = true
-        reverseButton.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 16).isActive = true
-        reverseButton.trailingAnchor.constraint(equalTo: parent.trailingAnchor, constant: -16).isActive = true
-        reverseButton.bottomAnchor.constraint(equalTo: parent.bottomAnchor, constant: -66).isActive = true
-        
-        reverseWordsLabel.translatesAutoresizingMaskIntoConstraints = false
-        reverseWordsLabel.heightAnchor.constraint(equalToConstant: 41).isActive = true
-        reverseWordsLabel.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 16).isActive = true
-        reverseWordsLabel.trailingAnchor.constraint(equalTo: parent.trailingAnchor, constant: -16).isActive = true
-        reverseWordsLabel.topAnchor.constraint(equalTo: parent.topAnchor, constant: 152).isActive = true
-        
-        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
-        descriptionLabel.heightAnchor.constraint(equalToConstant: 44).isActive = true
-        descriptionLabel.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 16).isActive = true
-        descriptionLabel.trailingAnchor.constraint(equalTo: parent.trailingAnchor, constant: -16).isActive = true
-        descriptionLabel.topAnchor.constraint(equalTo: parent.topAnchor, constant: 209).isActive = true
-        
-        textInput.translatesAutoresizingMaskIntoConstraints = false
-        textInput.heightAnchor.constraint(equalToConstant: 22).isActive = true
-        textInput.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 16).isActive = true
-        textInput.trailingAnchor.constraint(equalTo: parent.trailingAnchor, constant: -16).isActive = true
-        textInput.centerYAnchor.constraint(equalTo: parent.centerYAnchor, constant: 0).isActive = true
-        
-        textOutput.translatesAutoresizingMaskIntoConstraints = false
-        textOutput.widthAnchor.constraint(equalToConstant: 184).isActive = true
-        textOutput.heightAnchor.constraint(equalToConstant: 22).isActive = true
-        textOutput.leadingAnchor.constraint(equalTo: parent.leadingAnchor, constant: 16).isActive = true
-        textOutput.topAnchor.constraint(equalTo: parent.topAnchor, constant: 450).isActive = true
-
-
-    }
-
-
+    internal func position(for bar: UIBarPositioning) -> UIBarPosition { return .topAttached }
 }
-
-extension UITextField {
-
-    func setUnderLine(color:CGColor) {
-        let border = CALayer()
-        let width = CGFloat(0.5)
-        border.borderColor = color
-        border.frame = CGRect(x: 0, y: self.frame.size.height - width, width:  self.frame.size.width - 10, height: self.frame.size.height)
-        border.borderWidth = width
-        self.layer.addSublayer(border)
-        self.layer.masksToBounds = true
-    }
-
-}
-
