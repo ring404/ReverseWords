90ffb7a3124c93f6d58a11b117f09f121a5ef220
Dimitreee <Dmitrii.priimuk@gmail.com>
2022-02-23 11:17:48 +0300

    ,

---
+++ Pods/Target Support Files/Pods-ReverseWords-ReverseWordsUITests/Pods-ReverseWords-ReverseWordsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReverseWords_ReverseWordsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_ReverseWords_ReverseWordsUITests
+@end
---
+++ Pods/Target Support Files/Pods-ReverseWordsTests/Pods-ReverseWordsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ReverseWordsTests : NSObject
+@end
+@implementation PodsDummy_Pods_ReverseWordsTests
+@end
